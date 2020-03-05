8dc0b7f969720ef7dff652783ab11a480f2406d2
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-05 06:33:44 +0300

    Added basic actions and designed a post action.

---
+++ BirthdayApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using BirthdayApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult InvitationForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InvitationForm(InvitationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                DataBase.Add(model);
+                return View("Thanks", model);
+            }
+
+            return View(model);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Attendants()
+        {
+            var attendants = DataBase.List.Where(i => i.AttendanceStatus == true);
+
+            return PartialView(attendants);
+        }
+    }
+}
