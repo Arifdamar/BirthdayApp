a46c92e14421d245392d831f1520ea7bddcb5ea7
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-05 06:32:05 +0300

    Added and designed basic mvc views.

---
+++ BirthdayApp/Models/InvitationModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BirthdayApp.Models
+{
+    public class InvitationModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public bool? AttendanceStatus { get; set; }
+    }
+}
