ed1e579e285150cfa87fcb8e1711d56493d2c06b
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-05 14:37:13 +0300

    Added web api controller.

---
+++ BirthdayApp/Controllers/InvitationController.cs
@@ -0,0 +1,30 @@
+﻿using BirthdayApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BirthdayApp.Controllers
+{
+    public class InvitationController : ApiController
+    {
+        public IEnumerable<InvitationModel> GetAttendants()
+        {
+            return DataBase.List.Where(i => i.AttendanceStatus == true);
+        }
+
+        public IEnumerable<InvitationModel> GetNonAttendants()
+        {
+            return DataBase.List.Where(i => i.AttendanceStatus == false);
+        }
+        public void PostAdd(InvitationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                DataBase.Add(model);
+            }
+        }
+    }
+}
