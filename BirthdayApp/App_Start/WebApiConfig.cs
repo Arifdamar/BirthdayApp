c25507cd456c9803604dd619cba38c33c27ffc32
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-05 14:52:03 +0300

    Changed data format xml to json.

--- BirthdayApp/App_Start/WebApiConfig.cs
+++ BirthdayApp/App_Start/WebApiConfig.cs
@@ -19,6 +19,8 @@ namespace BirthdayApp
                 routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
